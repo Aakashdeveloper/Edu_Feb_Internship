757f384a6f99f0a8bf45ad870825def2d0048a0e
aakashdeveloper <ahanda205@gmail.com>
2021-02-20 23:05:05 +0530

    hh

---
+++ day5/five.js
@@ -0,0 +1,48 @@
+=  assignment
+== compare data only
+=== compare data and datatype
+
+var a = 10
+var b = "10"
+a==b
+true
+a===b
+false
+
+10>2
+10<2
+10>=2
+10<=2
+
+var a = true
+undefined
+!a
+false
+var b = false
+undefined
+!b
+true
+
+var a = 1
+var b = 0
+!a
+!b
+
+!a
+false
+!b
+true
+
+var c = -1
+undefined
+!c
+false
+var a = "hi"
+undefined
+!a
+false
+
+truthy = any number weather +ve or -ve except 0, string, true all are truthy
+falsy > 0, null,undefined,""
+
+
